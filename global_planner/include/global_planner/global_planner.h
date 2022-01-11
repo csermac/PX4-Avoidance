83e9924ace7a11ff15392237a1e1043d34aaf941
Davide <davide.patria@gmail.com>
2022-01-11 16:41:55 +0100

    tuning of a parameter related to error messages

--- global_planner/include/global_planner/global_planner.h
+++ global_planner/include/global_planner/global_planner.h
@@ -67,7 +67,7 @@ class GlobalPlanner {
   // Dynamic reconfigure parameters
   int min_altitude_ = 1;
   int max_altitude_ = 10;
-  double max_cell_risk_ = 0.2;
+  double max_cell_risk_ = 0.5;
   double smooth_factor_ = 10.0;
   double vert_to_hor_cost_ = 1.0;  // The cost of changing between vertical and
                                    // horizontal motion (TODO: use it)
